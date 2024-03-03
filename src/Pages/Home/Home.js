72c2c8aec425edd553c72f229e28b7da542338fc
uwais hasan <owies.h.hassan@gmail.com>
2024-03-03 13:12:59 +0200

    update img

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -57,7 +57,7 @@ const Home = () => {
                    {/*<motion.span variants={snake} animate='visible' custom={3}></motion.span>*/}
                    {/*<motion.span variants={snake} animate='visible' custom={4}></motion.span>*/}
 
-                   <p className='style-title'>hello my name is Uwais hasan</p>
+                   <p  className='style-title'>hello my name is <span style={ style.color}>Uwais hasan</span></p>
                    <p className='style-title'>I am a
                        <span style={ style.color}>
                            <Typewriter
